0a96cd20e585798f19901cf6df78a3ed5f4d03b8
daanrs <git@daan.rs>
2023-08-23 11:46:20 +0200

    treewide: add hakyll template

--- flake.nix
+++ flake.nix
@@ -2,57 +2,60 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+
     haskell-flake.url = "github:srid/haskell-flake";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
       # systems = nixpkgs.lib.systems.flakeExposed;
       systems = ["x86_64-linux"];
-      imports = [ inputs.haskell-flake.flakeModule ];
+      imports = [
+        inputs.haskell-flake.flakeModule
+        inputs.treefmt-nix.flakeModule
+      ];
 
-      perSystem = { self', pkgs, ... }: {
-
-        # Typically, you just want a single project named "default". But
-        # multiple projects are also possible, each using different GHC version.
+      perSystem = {
+        self',
+        pkgs,
+        config,
+        ...
+      }: {
         haskellProjects.default = {
-          # The base package set representing a specific GHC version.
-          # By default, this is pkgs.haskellPackages.
-          # You may also create your own. See https://zero-to-flakes.com/haskell-flake/package-set
-          # basePackages = pkgs.haskellPackages;
+          packages = {};
+          settings = {};
 
-          # Extra package information. See https://zero-to-flakes.com/haskell-flake/dependency
-          #
-          # Note that local packages are automatically included in `packages`
-          # (defined by `defaults.packages` option).
-          #
-          # packages = {
-          #   aeson.source = "1.5.0.0"; # Hackage version override
-          #   shower.source = inputs.shower;
-          # };
-          # settings = {
-          #   aeson = {
-          #     check = false;
-          #   };
-          #   relude = {
-          #     haddock = false;
-          #     broken = false;
-          #   };
-          # };
+          devShell.hlsCheck.enable = false;
 
-          # devShell = {
-          #  # Enabled by default
-          #  enable = true;
-          #
-          #  # Programs you want to make available in the shell.
-          #  # Default programs can be disabled by setting to 'null'
-          #  tools = hp: { fourmolu = hp.fourmolu; ghcid = null; };
-          #
-          #  hlsCheck.enable = true;
-          # };
+          autoWire = ["packages" "checks"];
         };
 
-        # haskell-flake doesn't set the default package, but you can do it here.
-        # packages.default = self'.packages.example;
+        treefmt.config = {
+          projectRootFile = "flake.nix";
+
+          programs.ormolu.enable = true;
+          programs.alejandra.enable = true;
+          programs.cabal-fmt.enable = true;
+          programs.hlint.enable = true;
+        };
+
+        devShells.default = pkgs.mkShell {
+          name = "default";
+          inputsFrom = [
+            config.haskellProjects.default.outputs.devShell
+            config.treefmt.build.devShell
+          ];
+          nativeBuildInputs = with pkgs; [
+            just
+          ];
+        };
       };
     };
 }
